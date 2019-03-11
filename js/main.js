d5b67d32a6eac7541744bb0f6c7296717e9dfb29
lanimaLv <42677016+lanimaLv@users.noreply.github.com>
2019-03-12 01:19:21 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
-import jump from "jump.js";
+var d = new Date();
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+document.getElementById("month").innerHTML = months[d.getMonth()];
 
-var button = document.getElementById("button");
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 15, 2019 23:59:59").getTime();
 
-button.addEventListener("click", () => {
-  jump(".que");
-});
+// Update the count down every 1 second
+var x = setInterval(function() {
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("time").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s " + "!";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "EXPIRED";
+  }
+}, 1000);
