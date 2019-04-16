6c97881aef641fb79c044e4fd4b06035e83867e0
lanimaLv <42677016+lanimaLv@users.noreply.github.com>
2019-04-16 14:20:08 +0300

    test

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var months = [
 document.getElementById("month").innerHTML = months[d.getMonth()];
 
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 15, 2019 23:59:59").getTime();
+var countDownDate = new Date("May 15, 2019 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
